2071f6c71a1fecd505f948e4979920e57d06cbe3
Davi Sampaio <sampaiodev23@gmail.com>
2018-06-20 13:47:45 -0300

    Teste de Inserção

--- src/main/java/com/davisampaio/mc/ModelagemConceitualApplication.java
+++ src/main/java/com/davisampaio/mc/ModelagemConceitualApplication.java
@@ -1,12 +1,31 @@
 package com.davisampaio.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemConceitualApplication {
+import com.davisampaio.mc.domain.Categoria;
+import com.davisampaio.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemConceitualApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));		
 	}
 }
