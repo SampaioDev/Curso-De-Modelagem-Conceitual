b30f950c55f8f451b2a6929015a60b554e98a9e0
Davi Sampaio <sampaiodev23@gmail.com>
2018-07-10 14:17:02 -0300

    comando DELETE feito para o endpoint /categorias

--- src/main/java/com/davisampaio/mc/services/CategoriaService.java
+++ src/main/java/com/davisampaio/mc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.davisampaio.mc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.davisampaio.mc.domain.Categoria;
 import com.davisampaio.mc.repositories.CategoriaRepository;
+import com.davisampaio.mc.services.exception.DataIntegrityException;
 import com.davisampaio.mc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
